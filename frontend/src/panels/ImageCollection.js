415a304f8b44709366b4dd5c429cfe82219b1775
Oliver Norton <onorton125@gmail.com>
2018-01-31 20:15:23 +0000

    When user selects an image, it is saved in state and that image is highlighted.

--- frontend/src/panels/ImageCollection.js
+++ frontend/src/panels/ImageCollection.js
@@ -6,23 +6,28 @@ export default class ImageCollection extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      imageUrls: [],
+      imageUrls: ['http://naturaldogguide.com/wp-content/uploads/2016/10/Ring-Worm-in-Dogs.jpg','http://purrfectcatbreeds.com/wp-content/uploads/2014/06/Chartreux4.jpg','https://bransbyhorses.co.uk/app/uploads/2016/03/Sophie.jpg'],
       selected: 0
     };
   }
 
+  select(index) {
+    this.setState({selected:index})
+  }
 
   render(){
+    let collection = this;
     let selected = this.state.selected;
     let imageStyle = {maxHeight: '300px', maxWidth: '90%'}
-    let selectedStyle = { borderColor: '#005ce6', borderStyle: 'solid', maxHeight: '300px', maxWidth: '90%'}
+    let selectedStyle = { outlineColor: '#005ce6', outlineStyle: 'solid', maxHeight: '300px', maxWidth: '90%'}
+
     return (
       <div style={{width: '300px', height: '100%', backgroundColor: '#CCCCCC'}}>
         <Scrollbars style={{ height: 780 }}>
          <ul style={{listStyleType: 'none', padding: 0, margin: 0}}>
            {
              this.state.imageUrls.map(function(url, index){
-           return <li style={{padding: '5px 0px 5px 0px'}}><img src={url} style={(index == selected) ? selectedStyle : imageStyle} /></li>;
+           return <li style={{padding: '5px 0px 5px 0px'}}><img src={url} onClick={() => collection.setState({selected: index})} style={(index == selected) ? selectedStyle : imageStyle} /></li>;
                    })}
            </ul>
         </Scrollbars>
