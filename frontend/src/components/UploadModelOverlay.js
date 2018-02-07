24ff04d85bdc4bc5ca2d08ca81a4e385982d66f8
Oliver Norton <onorton125@gmail.com>
2018-02-07 19:28:40 +0000

    Added type checking for adding files for classification models.

--- frontend/src/components/UploadModelOverlay.js
+++ frontend/src/components/UploadModelOverlay.js
@@ -7,10 +7,26 @@ export default class UploadModelOverlay extends Component {
     super(props);
     this.state = {
       isOpen: true,
-      validArchitecture: false,
+      validArchitecture: true,
       validModel: true,
       validLabels: true,
     };
+  }
+  isValidFile(filename, type) {
+    var parts = filename.split('.');
+    const ext = parts[parts.length - 1];
+    return ext.toLowerCase() == type
+  }
+
+  addArchitecture(file) {
+    this.setState({validArchitecture: this.isValidFile(file.name,'prototxt')})
+  }
+
+  addModel(file) {
+    this.setState({validModel: this.isValidFile(file.name,'caffemodel')})
+  }
+  addModel(file) {
+    this.setState({validLabels: this.isValidFile(file.name,'txts')})
   }
 
   render(){
@@ -21,17 +37,17 @@ export default class UploadModelOverlay extends Component {
               <p>If this is the first time using Deep Introspection you need to upload a caffe model to analyse.</p>
               <label class="pt-label">
               <h5>Architecture File</h5>
-              {(this.state.validArchitecture) ? <br/> : <div/>}
               <p>This is the *.prototxt file that defines the architecture of the network.</p>
               {(this.state.validArchitecture) ? <div/>: <p style={{color:'#DB3737'}}>Must be a prototxt file.</p>}
               <label class="pt-file-upload">
-              <input type="file" accept=".prototxt"/>
+              <input type="file" accept=".prototxt" onChange={(event) => this.addArchitecture(event.target.files[0])}/>
               <span class="pt-file-upload-input">Choose file...</span>
               </label>
             </label>
             <label class="pt-label">
             <h5>Weights File</h5>
             <p>This is the file containing the weights and biases of the network.</p>
+            {(this.state.validModel) ? <div/>: <p style={{color:'#DB3737'}}>Must be a caffemodel file.</p>}
             <label class="pt-file-upload">
             <input type="file" accept=".caffemodel"/>
             <span class="pt-file-upload-input">Choose file...</span>
@@ -41,6 +57,7 @@ export default class UploadModelOverlay extends Component {
           <label class="pt-label">
           <h5>Class Labels </h5>
           <p>Class labels for the network. Though not required, it makes it easy to see what class the network recognises. This should be a file with a line for each class name.</p>
+          {(this.state.validLabels) ? <div/>: <p style={{color:'#DB3737'}}>Must be a text file.</p>}
           <label class="pt-file-upload">
           <input type="file" accept=".txt"/>
           <span class="pt-file-upload-input">Choose file...</span>
