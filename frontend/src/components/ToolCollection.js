a629fdd67ba871a8b54fccc71877250d6bf6f8db
Oliver Norton <onorton125@gmail.com>
2018-02-09 17:37:30 +0000

    Added SynthesisTool component.

--- frontend/src/components/ToolCollection.js
+++ frontend/src/components/ToolCollection.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import { Tab2, Tabs2 } from "@blueprintjs/core";
 import OcclusionTool from './OcclusionTool'
+import SynthesisTool from './SynthesisTool'
 export default class ToolCollection extends Component {
 
   render(){
     return (
     <Tabs2 id="toolTabs" onChange={this.handleTabChange} style={this.props.style}>
    <Tab2 id="oc" title={<h5>Occlusion</h5>} panel={<OcclusionTool testImage={this.props.testImage}/>} />
-   <Tab2 id="sy" title={<h5>Synthesis</h5>} panel={<p>Synthesis panel</p>}/>
+   <Tab2 id="sy" title={<h5>Synthesis</h5>} panel={<SynthesisTool testImage={this.props.testImage}/>}/>
    <Tabs2/>
 </Tabs2>
     )
