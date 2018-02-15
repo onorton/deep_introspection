f3009fa4753e2199f7a3eeba0dd235f342c3e371
Oliver Norton <onorton125@gmail.com>
2018-02-15 16:13:35 +0000

    Added function that propagates relevances for fully-connected layers naively

--- deep_introspection/lrp.py
+++ deep_introspection/lrp.py
@@ -1,12 +1,10 @@
 import numpy as np
 
 def get_layer_names(net) :
-    """Calculates the layer-wise relevance propagations for a given im
-    inputs
-    img: Test image as numpy array
+    """Gets the layer names of relevant networks in order
     net: caffe network
     output
-    outgoing discriminative locations of current layer
+    list of layer names
     """
 
     layer_names = []
@@ -16,3 +14,42 @@ def get_layer_names(net) :
             layer_names.append(net._layer_names[i])
 
     return layer_names
+
+
+def propagate_fully_connected(relevances, weights, activations, alpha):
+    """Calculates the layer-wise relevance propagations for a given fully-connected layer
+    inputs
+    relevances: relevances of higher layer
+    weights: weights from higher layer to current
+    activations: activations for current layer
+    alpha: value of weighting positive and negative weights
+    output
+    relevances of current layer
+    """
+    beta = alpha - 1
+    number_current_neurons = weights.shape[0]
+    number_higher_neurons = weights.shape[1]
+    relevances_current = np.zeros(number_current_neurons)
+
+    for j in range(number_current_neurons):
+        for k in range(number_higher_neurons):
+            if weights[j][k] >= 0:
+                # calculate positive component
+                positive = weights[j][k]*activations[j]
+                sum_mults = 0
+                for i in range(number_current_neurons):
+                    if weights[i][k] >= 0:
+                        sum_mults += weights[i][k]*activations[i]
+                positive /= sum_mults
+                relevances_current[j] += relevances[k]*alpha*positive
+            else:
+                negative = weights[j][k]*activations[j]
+                sum_mults = 0
+                for i in range(number_current_neurons):
+                    if weights[i][k] < 0:
+                        sum_mults += weights[i][k]*activations[i]
+
+                negative/= sum_mults
+                relevances_current[j] -= relevances[k]*beta*negative
+    print(relevances_current)
+    return relevances_current
--- deep_introspection/test/test_lrp.py
+++ deep_introspection/test/test_lrp.py
@@ -1,4 +1,5 @@
 from deep_introspection import lrp
+import numpy as np
 import caffe
 
 def test_layers_found_should_be_in_order():
@@ -7,3 +8,10 @@ def test_layers_found_should_be_in_order():
     net = caffe.Net('deep_introspection/test/VGG.prototxt',caffe.TEST)
     layer_names = lrp.get_layer_names(net)
     assert(layer_names == ['conv1_1','conv1_2','pool1','conv2_1','conv2_2','pool2','conv3_1','conv3_2','conv3_3','pool3','conv4_1','conv4_2','conv4_3','pool4','conv5_1','conv5_2','conv5_3','pool5','fc6','fc7','fc8'])
+
+def test_relevances_propagated_fully_connected_layers():
+    # Example from a 2 neuron fully connected layer to a 2 neuron fully connected layer
+    relevances = np.array([0.5, 0.4])
+    weights = np.array([[0.2, -1],[0.3, -0.7]])
+    activations = np.array([0.1, 0.4])
+    assert(np.isclose(lrp.propagate_fully_connected(relevances, weights, activations, 0.5), np.array([0.08834586,0.36165414])).all())
