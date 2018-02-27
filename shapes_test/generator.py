232cce33748c147ff8716f6edd843b56377b68ae
Oliver Norton <onorton125@gmail.com>
2018-02-27 16:54:18 +0000

    Shapes are transformed by a (now constant) matrix.

--- shapes_test/generator.py
+++ shapes_test/generator.py
@@ -4,6 +4,9 @@ from skimage import util
 
 num = 10
 
+def random_transform_matrix():
+    return np.array([[1, 0, 0],[0, 1, 0],[0,0,1]])
+
 def generate_noisy_image():
     im = np.zeros(shape=(500,500,3))
     im = 255*util.random_noise(im, mode='gaussian')
@@ -11,11 +14,17 @@ def generate_noisy_image():
     return im
 
 def generate_quad():
-    points = np.array([[200,200],[300,200],[300,300],[200,300]])
+    points = np.array([[200,200,1],[300,200,1],[300,300,1],[200,300,1]])
+    points[:,0:1] -= 250
+    points = points.transpose()
+
+    m = random_transform_matrix()
+    points = np.matmul(m, points).transpose()
+    points[:,0:1] += 250
 
     point_list = []
     for p in points:
-        point_list.append((p[0],p[1]))
+        point_list.append((p[0]/p[2],p[1]/p[2]))
 
     im = generate_noisy_image()
     draw = ImageDraw.Draw(im)
@@ -23,12 +32,18 @@ def generate_quad():
     return im
 
 def generate_ellipse():
-    points = np.array([[200,200],[300,300]])
+    points = np.array([[200,200,1],[300,300,1]])
+    points[:,0:1] -= 250
+    points = points.transpose()
+
+    m = random_transform_matrix()
+    points = np.matmul(m, points).transpose()
+    points[:,0:1] += 250
 
     point_list = []
     for p in points:
-        point_list.append((p[0],p[1]))
-
+        point_list.append((p[0]/p[2],p[1]/p[2]))
+        
     im = generate_noisy_image()
     draw = ImageDraw.Draw(im)
     draw.ellipse(point_list, fill=(255,255,255))
