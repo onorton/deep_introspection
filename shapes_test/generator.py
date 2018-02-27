03855c194314bf07ca1165e0c31e37c44f1ff198
Oliver Norton <onorton125@gmail.com>
2018-02-27 16:27:17 +0000

    Now random noise is added to the images of quadrilaterals.

--- shapes_test/generator.py
+++ shapes_test/generator.py
@@ -1,13 +1,26 @@
 from PIL import Image, ImageDraw
+import numpy as np
+from skimage import util
 
 num = 10
 
+def generate_noisy_image():
+    im = np.zeros(shape=(500,500,3))
+    im = 255*util.random_noise(im, mode='gaussian')
+    im = Image.fromarray(np.uint8(im))
+    return im
+
+
 def generate_quad():
-    points = [(200,200),(300,200),(300,300),(200,300)]
-    im = Image.new('RGB', (500, 500), (255, 255, 255))
+    points = np.array([[200,200],[300,200],[300,300],[200,300]])
+
+    point_list = []
+    for p in points:
+        point_list.append((p[0],p[1]))
+
+    im = generate_noisy_image()
     draw = ImageDraw.Draw(im)
-    for p in range(len(points)):
-        draw.line(points[p%len(points)]+points[(p+1)%len(points)], fill=(0,0,0), width=3)
+    draw.polygon(point_list, fill=(255,255,255))
     return im
 
 for i in range(num):
