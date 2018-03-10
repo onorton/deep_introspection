4729651226e08b8d845adb5a726343111b180bd7
Oliver Norton <onorton125@gmail.com>
2018-03-10 19:32:37 +0000

    Features are displayed and can be toggled.

--- frontend/src/components/OcclusionTool.js
+++ frontend/src/components/OcclusionTool.js
@@ -25,7 +25,9 @@ export default class OcclusionTool extends Component {
     }).then(function(response) {
       if (response.status == 200) {
         response.json().then(function(data) {
-          tool.setState({features: data.features})
+          const features = data.features.map(function(feature) {return {feature: feature, active: true}})
+          console.log(features)
+          tool.setState({features: features})
         })
       }
 
@@ -33,9 +35,25 @@ export default class OcclusionTool extends Component {
       console.log('There has been a problem with your fetch operation: ' + error.message);
     });
   }
+
+  toggle(index) {
+    var features = this.state.features
+    features[index].active = !features[index].active
+    this.setState({features: features})
+  }
   render(){
+    const tool = this
+
     return (
     <div className="toolArea">
+    <ul style={{listStyleType: 'none', padding: 0, margin: 0, float:'left'}}>
+    {(this.state.features != null) ? this.state.features.map(function(feature, index) {
+
+         const activeFeature = <li style={{padding: '5px 0px 5px 0px'}}><label className="pt-button pt-active pt-intent-primary " onClick={() => tool.toggle(index)}>Feature {feature.feature}</label></li>
+         const inactiveFeature =  <li style={{padding: '5px 0px 5px 0px'}}><label className="pt-button pt-intent-primary" onClick={() => tool.toggle(index)}>Feature {feature.feature}</label></li>
+        return (feature.active) ? activeFeature : inactiveFeature
+    }): <div/>}
+    </ul>
     {(this.props.testImage != undefined) ? <img src={this.props.testImage.url} style={{maxWidth:300, maxHeight:300, float:"right", marginRight:20, borderStyle:"solid", borderColor:"#10161A"}}/> : <div/>}
     </div>
     )
