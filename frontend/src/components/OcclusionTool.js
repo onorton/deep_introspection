37b15c4c495ec9b283a5ee9cf70a06b14a7dc7df
Oliver Norton <onorton125@gmail.com>
2018-03-10 20:49:00 +0000

    Fixed componentWillReceiveProps() so feature buttons are updated immediately.

--- frontend/src/components/OcclusionTool.js
+++ frontend/src/components/OcclusionTool.js
@@ -5,19 +5,20 @@ export default class OcclusionTool extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      features: null
+      features: []
     };
   }
-  componentWillReceiveProps() {
-    this.fetchFeatures()
+  componentWillReceiveProps(nextProps) {
+    console.log("props received")
+    this.fetchFeatures(nextProps.testModel.id, nextProps.testImage.id)
   }
   componentDidMount() {
-    this.fetchFeatures()
+    this.fetchFeatures(this.props.testModel.id, this.props.testImage.id)
   }
 
-  fetchFeatures() {
+  fetchFeatures(model, image) {
     const tool = this
-    fetch('http://127.0.0.1:8000/features/' + this.props.testModel.id + '/' + this.props.testImage.id, {
+    fetch('http://127.0.0.1:8000/features/' + model + '/' + image, {
       method: 'GET',
       headers: {
           "Content-Type": "application/json"
@@ -25,8 +26,8 @@ export default class OcclusionTool extends Component {
     }).then(function(response) {
       if (response.status == 200) {
         response.json().then(function(data) {
+          console.log(data.features)
           const features = data.features.map(function(feature) {return {feature: feature, active: true}})
-          console.log(features)
           tool.setState({features: features})
         })
       }
@@ -40,21 +41,24 @@ export default class OcclusionTool extends Component {
     var features = this.state.features
     features[index].active = !features[index].active
     this.setState({features: features})
+    const inactiveFeatures = features.filter(feature => !feature.active).map(feature => feature.feature)
   }
   render(){
     const tool = this
 
     return (
     <div className="toolArea">
-    <ul style={{listStyleType: 'none', padding: 0, margin: 0, float:'left'}}>
-    {(this.state.features != null) ? this.state.features.map(function(feature, index) {
+    <ul style={{listStyleType: 'none', padding: 0, marginLeft:10, float:'left'}}>
+    {this.state.features.map(function(feature, index) {
 
          const activeFeature = <li style={{padding: '5px 0px 5px 0px'}}><label className="pt-button pt-active pt-intent-primary " onClick={() => tool.toggle(index)}>Feature {feature.feature}</label></li>
          const inactiveFeature =  <li style={{padding: '5px 0px 5px 0px'}}><label className="pt-button pt-intent-primary" onClick={() => tool.toggle(index)}>Feature {feature.feature}</label></li>
         return (feature.active) ? activeFeature : inactiveFeature
-    }): <div/>}
+    })}
     </ul>
-    {(this.props.testImage != undefined) ? <img src={this.props.testImage.url} style={{maxWidth:300, maxHeight:300, float:"right", marginRight:20, borderStyle:"solid", borderColor:"#10161A"}}/> : <div/>}
+    <div className="results" style={{ float:"right", marginRight:20}}>
+    {(this.props.testImage != undefined) ? <img src={this.props.testImage.url} style={{maxWidth:300, maxHeight:300, borderStyle:"solid", borderColor:"#10161A"}}/> : <div/>}
+    </div>
     </div>
     )
   }
