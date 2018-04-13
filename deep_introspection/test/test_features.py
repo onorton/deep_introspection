fa5e2dac138eb9e5c20496cbb07154b25018c920
Oliver Norton <onorton125@gmail.com>
2018-04-13 22:35:02 +0100

    Fixed issue with test not using CaffeNet class.

--- deep_introspection/test/test_features.py
+++ deep_introspection/test/test_features.py
@@ -1,6 +1,6 @@
 from deep_introspection import features
 from deep_introspection import lrp
-from deep_introspection import utils
+from deep_introspection import utils, network
 
 import numpy as np
 
@@ -14,10 +14,10 @@ def test_captures_disctinct_features():
     caffe.set_device(0)  # if we have multiple GPUs, pick the first one
     caffe.set_mode_gpu()
 
-    net = caffe.Classifier('deep_introspection/test/VGG.prototxt', 'deep_introspection/test/VGG_ILSVRC_16_layers.caffemodel', caffe.TEST,channel_swap=(2,1,0))
+    net = network.CaffeNet('deep_introspection/test/VGG.prototxt', 'deep_introspection/test/VGG_ILSVRC_16_layers.caffemodel')
 
     img, offset, resFac, newSize = utils.imgPreprocess(img_path='deep_introspection/test/cat.jpg')
-    net.image_dims = newSize
+    net.set_new_size(newSize)
     relevances = lrp.calculate_lrp_heatmap(net, img)
 
     clusters = features.extract_features_from_relevances(relevances)
