Fixed issue with ReLUs when calculating gradient.

deep_introspection/synthesis.py
@@ -8,19 +8,18 @@ l_tv = 10
 l = 8e-10
 m = 0.5
 
-def synthesise(net, rep):
+def synthesise(net, target):
     """
     Find an image whose representation in the network is as close as possible
     to the representation given.
 
     inputs
     net: the network that is being used
-    rep: the representation that is trying to be inverted
+    target: the representation that is trying to be inverted
     in this case, the layer before the final softmax layer
     output
     An image as a numpy array, total loss
     """
-
     x = 256*np.random.uniform(size=net.input_shape())-128
     layer = net.get_layer_names()[-2]
     net.set_new_size(x.shape[:2])
@@ -29,32 +28,35 @@ def synthesise(net, rep):
     mu = 0
     prev_x = np.copy(x)
     net.predict(x)
-    rep_loss = loss(net.get_activations(layer), rep)
-    prev_loss = rep_loss
+    rep_loss = loss(net.get_activations(layer), target)
+    prev_loss = rep_loss + regularised(x)
 
     print("Initial loss: " + str(rep_loss))
 
-    for i in range(10):
-        grad = gradient(net, (rep_loss-rep)/np.linalg.norm(rep))
-        delta = x*grad + l*alpha*x**(alpha-1) + l_tv*tv_grad(x)
+    for i in range(100):
+        grad = gradient(net, (net.get_activations(layer)-target))
+        delta = grad + l*alpha*x**(alpha-1) + l_tv*tv_grad(x)
+        #delta = grad
+        old_mu = np.copy(mu)
         mu = m*mu + (1-m)*-lr * delta
-
         x += mu
 
         net.predict(x)
-        rep_loss = loss(net.get_activations(layer), rep)
-        print("Total loss: " + str(rep_loss+regularised(x)))
+        rep_loss = loss(net.get_activations(layer), target)
+        print("Iteration " + str(i) +": " + str(lr))
+        print("Total loss:" + str(rep_loss+regularised(x)))
         print("Loss: " + str(rep_loss))
 
         if rep_loss + regularised(x) <= prev_loss:
-            if lr < 0.5:
-                lr *= 2
+            lr *= 2
             prev_x = np.copy(x)
             prev_loss = rep_loss + regularised(x)
         else:
             lr /= 2
             x = np.copy(prev_x)
+            mu = old_mu
 
+    print(x.flatten())
     return x+128, (rep_loss + regularised(x))
 
 def regularised(x):
@@ -117,7 +119,7 @@ def gradient(net, out):
     layer_names = net.get_layer_names()[:-1]
     layer = net.get_layer_names()[-2]
 
-    grad = out
+    grad = 2*out
     #grad = (out/net.get_activations(layer).flatten().shape[0])*np.ones(net.get_activations(layer).shape)
     layer_names.reverse()
 
@@ -131,12 +133,15 @@ def gradient(net, out):
             grad = lrp.backwardMax(grad, net.get_activations(next_layer), kernel)
         elif layer_type == 'InnerProduct':
             next_layer_type = net.get_layer_type(next_layer)
-            grad = np.maximum(grad, 0)
+            without_relu = np.matmul(net.get_weights(name),net.get_activations(next_layer).flatten())+net.get_biases(name)
+            grad[without_relu <= 0] = 0
             grad = np.matmul(np.transpose(net.get_weights(name)),grad)
             if next_layer_type != 'InnerProduct' :
                 grad = grad.reshape(net.get_activations(next_layer).shape)
         elif layer_type == 'Convolution':
-            grad = np.maximum(grad, 0)
+            biases = (net.get_biases(name).T*np.ones(net.get_activations(name).shape).T).T
+            without_relu = lrp.forward(net.get_activations(next_layer), net.get_weights(name),net.get_activations(name).shape)[0]+biases
+            grad[without_relu <= 0] = 0
             grad = lrp.backprop(grad, net.get_weights(name), net.get_activations(next_layer))
 
     return grad.transpose(2,1,0)