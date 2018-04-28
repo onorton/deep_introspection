fixed bugs in forward and backward passes where dimensions were in the wrong order or the biases were not added.

deep_introspection/lrp.py
@@ -82,23 +82,27 @@ def propagate_first_conv(net, relevances, activations, weightsLayer, h, l):
 
     return X*backprop(s, weights, X) - L*backprop(s, positiveWeights, L) - H*backprop(s, negativeWeights, H)
 
-def forward(x, w, shape):
+def forward(x, w, b, shape):
     if len(x.shape) == 3:
         x = x.reshape((1,)+x.shape)
-    x_col = im2col.im2col_indices(x, w.shape[2], w.shape[3])
-    w_col = w.reshape(w.shape[0],w.shape[1]*w.shape[2]*w.shape[3])
+    w = w.transpose(0, 1, 3, 2)
 
-    out = np.matmul(w_col,x_col)
+    x_col = im2col.im2col_indices(x, w.shape[2], w.shape[3])
+    w_col = w.reshape(w.shape[0],-1)
+
+    out = ((w_col @ x_col).T + b.T).T
     out = out.reshape((shape[0], shape[1], shape[2], 1))
-    out = out.transpose(3, 0, 1, 2)
+    out = out.transpose(3, 0, 2, 1)
     return out
 
 def backprop(s, w, x):
     s_reshaped = s
     if len(s.shape) == 3:
         s_reshaped = s.reshape(tuple([1]+list(s.shape)))
-    s_reshaped = np.transpose(s_reshaped, (1,2,3,0))
+    s_reshaped = np.transpose(s_reshaped, (1,3,2,0))
     s_reshaped = s_reshaped.reshape(s_reshaped.shape[0],-1)
+
+    w = w.transpose(0,1,3,2)
 
     x = x.reshape(tuple([1]+list(x.shape)))
 
@@ -141,6 +145,7 @@ def forwardMax(x,k, shape):
 def backwardMax(s, x, k):
     x_reshaped = x.reshape((1,)+x.shape)
     x_reshaped = x_reshaped.reshape(x_reshaped.shape[1], 1, x_reshaped.shape[2], x_reshaped.shape[3])
+
     x_col = im2col.im2col_indices(x_reshaped, k, k, padding=0, stride=k)
 
     max_idx = np.argmax(x_col, axis=0)

deep_introspection/synthesis.py
@@ -4,9 +4,9 @@ import matplotlib.pyplot as plt
 
 alpha = 6
 beta = 2
-l_tv = 10
+l_tv = 200
 l = 8e-10
-m = 0.9
+m = 0.5
 
 def synthesise(net, target):
     """
@@ -21,37 +21,48 @@ def synthesise(net, target):
     An image as a numpy array, total loss
     """
     x = 256*np.random.uniform(size=net.input_shape())-128
-    layer = net.get_layer_names()[-2]
+    layer = net.get_layer_names()[-1]
     net.set_new_size(x.shape[:2])
 
-    lr = 0.01
+    initial_lr = 0.001
+    lr = initial_lr
     mu = 0
     prev_x = np.copy(x)
-    net.predict(x)
-    rep_loss = loss(net.get_activations(layer), target)
 
-    # prev_loss = rep_loss + regularised(x)
+    net.predict(x)
+    rep = net.get_activations(layer)
+
+    rep_loss = loss(rep, target)
+
+    #prev_loss = rep_loss
     prev_loss = rep_loss + regularised(x)
 
+    print("Initial total loss: " + str(prev_loss))
     print("Initial loss: " + str(rep_loss))
-    iterations = 400
+
+    iterations = 100
     for i in range(iterations):
-        grad = gradient(net, net.get_activations(layer)-target)
+        grad = gradient(net, rep-target)
         delta = grad + l*alpha*x**(alpha-1) + l_tv*tv_grad(x)
         #delta = grad
+
         old_mu = np.copy(mu)
         mu = -lr * delta
         x += mu
 
+
         net.predict(x)
-        rep_loss = loss(net.get_activations(layer), target)
+        rep = net.get_activations(layer)
+        rep_loss = loss(rep, target)
+
         total_loss = rep_loss + regularised(x)
         print("Iteration " + str(i) +": " + str(lr))
         print("Total loss:" + str(total_loss))
         print("Loss: " + str(rep_loss))
 
         if total_loss <= prev_loss:
-            lr *= 2
+            if lr < initial_lr:
+                lr *= 2
             prev_x = np.copy(x)
             prev_loss = total_loss
         else:
@@ -59,8 +70,7 @@ def synthesise(net, target):
             x = np.copy(prev_x)
             mu = old_mu
 
-    print(x.flatten())
-    return x+128, (rep_loss + regularised(x))
+    return x+128, total_loss
 
 def regularised(x):
 
@@ -112,18 +122,15 @@ def loss(rep, target):
     """
     return np.linalg.norm(rep-target)**2/(np.linalg.norm(target)**2)
 
-
 def gradient(net, out):
     """
     Computes the gradient of the given out
 
 
     """
-    layer_names = net.get_layer_names()[:-1]
-    layer = net.get_layer_names()[-2]
+    layer_names = net.get_layer_names()
 
     grad = 2*out
-    #grad = (out/net.get_activations(layer).flatten().shape[0])*np.ones(net.get_activations(layer).shape)
     layer_names.reverse()
 
     for index in range(len(layer_names)-1):
@@ -142,9 +149,8 @@ def gradient(net, out):
             if next_layer_type != 'InnerProduct' :
                 grad = grad.reshape(net.get_activations(next_layer).shape)
         elif layer_type == 'Convolution':
-            biases = (net.get_biases(name).T*np.ones(net.get_activations(name).shape).T).T
-            without_relu = lrp.forward(net.get_activations(next_layer), net.get_weights(name),net.get_activations(name).shape)[0]+biases
+            without_relu = lrp.forward(net.get_activations(next_layer), net.get_weights(name), net.get_biases(name), net.get_activations(name).shape)[0]
             grad[without_relu <= 0] = 0
             grad = lrp.backprop(grad, net.get_weights(name), net.get_activations(next_layer))
 
-    return grad.transpose(2,1,0)
+    return grad.T

deep_introspection/test/test_synthesis.py
@@ -5,10 +5,12 @@ import matplotlib.pyplot as plt
 def test_synthesise_loss_is_low():
     net = network.CaffeNet('deep_introspection/test/VGG.prototxt', 'deep_introspection/test/VGG_ILSVRC_16_layers.caffemodel')
     img, offset, resFac, newSize = utils.imgPreprocess(img_path='deep_introspection/test/cat.jpg')
+
     net.set_new_size(newSize)
     net.predict(img)
 
-    layer = net.get_layer_names()[-2]
+    layer = net.get_layer_names()[-1]
+    print("Testing layer " + layer)
 
     target_rep = net.get_activations(layer)
 
@@ -16,7 +18,7 @@ def test_synthesise_loss_is_low():
     net.set_new_size(solution.shape[:2])
     net.predict(solution)
 
-    plt.imshow(np.maximum(solution, 0)/256)
+    plt.imshow(np.maximum(solution, 0)/255)
     plt.show()
     print(loss)
 