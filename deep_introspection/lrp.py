Fixed backwards prop so that gradients would not be flipped every convolutional layer.

deep_introspection/lrp.py
@@ -99,19 +99,17 @@ def backprop(s, w, x):
     s_reshaped = s
     if len(s.shape) == 3:
         s_reshaped = s.reshape(tuple([1]+list(s.shape)))
-    s_reshaped = np.transpose(s_reshaped, (1,3,2,0))
+    s_reshaped = s_reshaped.transpose(1,3,2,0)
     s_reshaped = s_reshaped.reshape(s_reshaped.shape[0],-1)
 
-    w = w.transpose(0,1,3,2)
+    w = w.transpose(0, 1, 3, 2)
 
     x = x.reshape(tuple([1]+list(x.shape)))
-
-    x_col = im2col.im2col_indices(x, w.shape[2], w.shape[3])
 
     W_reshape = w.reshape(w.shape[0],-1)
     dX_col = W_reshape.T @ s_reshaped
     dX = im2col.col2im_indices(dX_col, x.shape, w.shape[2], w.shape[3])
-    return dX[0]
+    return dX[0].transpose(0,2,1)
 
 def propagate_pooling(net, relevances, activations, poolLayer, k):
     """Calculates the layer-wise relevance propagations for a given pooling layer

deep_introspection/synthesis.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 
 alpha = 6
 beta = 2
-l_tv = 200
-l = 8e-10
+l_tv = 400
+l = 1e-9
 m = 0.5
 
 def synthesise(net, target):
@@ -24,7 +24,7 @@ def synthesise(net, target):
     layer = net.get_layer_names()[-1]
     net.set_new_size(x.shape[:2])
 
-    initial_lr = 0.001
+    initial_lr = 0.0001
     lr = initial_lr
     mu = 0
     prev_x = np.copy(x)
@@ -40,7 +40,7 @@ def synthesise(net, target):
     print("Initial total loss: " + str(prev_loss))
     print("Initial loss: " + str(rep_loss))
 
-    iterations = 100
+    iterations = 40
     for i in range(iterations):
         grad = gradient(net, rep-target)
         delta = grad + l*alpha*x**(alpha-1) + l_tv*tv_grad(x)
@@ -60,11 +60,11 @@ def synthesise(net, target):
         print("Total loss:" + str(total_loss))
         print("Loss: " + str(rep_loss))
 
-        if total_loss <= prev_loss:
+        if rep_loss <= prev_loss:
             if lr < initial_lr:
                 lr *= 2
             prev_x = np.copy(x)
-            prev_loss = total_loss
+            prev_loss = rep_loss
         else:
             lr /= 2
             x = np.copy(prev_x)